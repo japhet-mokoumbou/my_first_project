772f8d1cf5a553825acf203b8a1959189b3750a9
japhet-mokoumbou <japhetmokoumbousekolet@gmail.com>
2023-02-19 13:32:55 +0100

    change main.py message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import paramiko
 import scp
+from selenium import webdriver
 
 # Se connecter à la machine distante via SSH
 ssh = paramiko.SSHClient()
@@ -21,6 +22,28 @@ client = scp.SCPClient(ssh.get_transport())
 client.get('/chemin/fichier1', '/chemin/local/fichier1')
 client.get('/chemin/fichier2', '/chemin/local/fichier2')
 
-# Fermer la connexion SCP et SSH
+#dans ce code nous voulons ouvrir le navigateur chrome
+# URL de la machine distante
+remote_url = "http://<adresse-ip-de-la-machine-distante>:<port>/wd/hub"
+
+# Options de configuration de Chrome pour la machine distante
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--headless')
+
+# Initialisation du navigateur sur la machine distante
+browser = webdriver.Remote(command_executor=remote_url, options=chrome_options)
+
+# Ouvrir Google Chrome
+browser.get("https://www.google.com")
+
+# Fermer le navigateur
+browser.quit()
+
+# Fermer la connexion SCP
 client.close()
+
+# Fermer la connexion SCP
 ssh.close()
